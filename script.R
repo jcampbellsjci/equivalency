1afbaa33f859af5216f45c7d45a9e2b70659f834
jcampbellsjci <jcampbellsjci@gmail.com>
2020-01-06 22:34:14 -0500

    Cleaning up and made stats per 36

--- script.R
+++ script.R
@@ -1,35 +1,43 @@
+#### G-League Equivalency ####
+
+# Loading up necessary packages
 library(rvest)
+library(Metrics)
 library(tidyverse)
 
-read_html("https://www.basketball-reference.com/gleague/years/gleague_2019_per_game.html") %>%
-  html_nodes('#per_game_stats') %>%
-  html_table() %>%
-  .[[1]] %>%
-  filter(Tm != "Tm") %>%
-  mutate_all(na_if, "") %>%
-  mutate_at(.vars = vars(Age:PTS), .funs = list(as.numeric))
-
+# Going to use map to loop through urls and pull g-leage data
 g_league <- map(.x = 2015:2019,
                 .f = ~ read_html(paste(
+                  # "https://www.basketball-reference.com/gleague/years/gleague_",
+                  # .x, "_per_game.html", sep = "")) %>%
                   "https://www.basketball-reference.com/gleague/years/gleague_",
-                  .x, "_per_game.html", sep = "")) %>%
-                  html_nodes('#per_game_stats') %>%
+                  .x, "_per_minute.html", sep = "")) %>%
+                  # html_nodes('#per_game_stats') %>%
+                  html_nodes('#per_minute_stats') %>%
                   html_table() %>%
                   .[[1]] %>%
+                  # Filtering out any header rows
                   filter(Tm != "Tm") %>%
                   mutate_all(na_if, "") %>%
-                  mutate_at(.vars = vars(Age:PTS), .funs = list(as.numeric)) %>%
+                  mutate_at(.vars = vars(Age:PTS),
+                            .funs = list(as.numeric)) %>%
                   distinct(Player, .keep_all = T)
                 )
+# Output is in a list
+# Name each element of the list to use as an ID
 names(g_league) <- 2015:2019
 g_league <- g_league %>%
   bind_rows(.id = "Season")
 
+# Performing looping for same timeframe for NBA data
 nba <- map(.x = 2015:2019,
                 .f = ~ read_html(paste(
+                  # "https://www.basketball-reference.com/leagues/NBA_",
+                  # .x, "_per_game.html", sep = "")) %>%
                   "https://www.basketball-reference.com/leagues/NBA_",
-                  .x, "_per_game.html", sep = "")) %>%
-                  html_nodes('#per_game_stats') %>%
+                  .x, "_per_minute.html", sep = "")) %>%
+                  # html_nodes('#per_game_stats') %>%
+                  html_nodes('#per_minute_stats') %>%
                   html_table() %>%
                   .[[1]] %>%
                   filter(Tm != "Tm") %>%
@@ -42,14 +50,21 @@ names(nba) <- 2015:2019
 nba <- nba %>%
   bind_rows(.id = "Season")
 
+# Combine both the NBA and G-League data
 combo <- nba %>%
   inner_join(g_league, by = c("Player", "Season")) %>%
+  # Any variable on the left table will be listed as NBA
   rename_at(.vars = vars(contains(".x")), 
             .funs = funs(sub(".x", "_nba", .))) %>%
+  # Any variable on the right table will be listed as G-League
   rename_at(.vars = vars(contains(".y")), 
             .funs = funs(sub(".y", "_gleague", .))) %>%
+  # Filtering specifically for players who played at least 10 games
+  # in each league in a season
   filter(G_nba >= 10 & G_gleague >= 10)
 
+# Calculating general ratios for stats
+# Shows how much a G-League stat is worth in the NBA
 ratios <- combo %>%
   mutate(PTS_RATIO = PTS_nba / PTS_gleague,
          AST_RATIO = AST_nba / AST_gleague,
@@ -58,13 +73,21 @@ ratios <- combo %>%
   summarize_at(.vars = vars(PTS_RATIO:REB_RATIO),
                .funs = list(mean))
 
-g_league_dist <- g_league %>%
-  filter(!(paste(Player, Season) %in% paste(combo$Player, combo$Season))) %>%
-  
-
+# Finding general NBA equivalent stats for all G-League players
 g_league_equiv <- g_league %>%
-  filter(!(paste(Player, Season) %in% paste(combo$Player, combo$Season))) %>%
   mutate(PTS_EQUIV = PTS * ratios$PTS_RATIO,
          AST_EQUIV = AST * ratios$AST_RATIO,
          REB_EQUIV = TRB * ratios$REB_RATIO) %>%
   select(Season, Player, PTS_EQUIV:REB_EQUIV)
+
+# Measuring accuracy of this generalized method
+accuracy_df <- combo %>%
+  select(Player, Season, PTS_nba, AST_nba, TRB_nba) %>%
+  inner_join(g_league_equiv, by = c("Player", "Season")) %>%
+  # Calculating RMSE for all players
+  summarize(PTS_accuracy = rmse(actual = PTS_nba,
+                                predicted = PTS_EQUIV),
+            AST_accuracy = rmse(actual = AST_nba,
+                                predicted = AST_EQUIV),
+            REB_accuracy = rmse(actual = TRB_nba,
+                                predicted = REB_EQUIV))
